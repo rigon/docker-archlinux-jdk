a94751d27dd075bb3b022d205ab979f883aa74de
Ricardo Gonçalves <ricar.goncalves@gmail.com>
2017-07-11 18:59:07 +0100

    Missing dependencies

--- setup-jdk.sh
+++ setup-jdk.sh
@@ -1,12 +1,12 @@
 # Dependencies for makepkg and JDK
-pacman -Sy --noconfirm binutils wget desktop-file-utils libxrender libxslt libxtst shared-mime-info hicolor-icon-theme java-environment-common java-runtime-common xdg-utils fakeroot
+pacman -Sy --noconfirm binutils wget desktop-file-utils libxrender libxslt libxtst shared-mime-info hicolor-icon-theme java-environment-common java-runtime-common xdg-utils fakeroot nss
 
 # Get and extract the JDK PKGBUILD
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/jdk.tar.gz
 tar xvf jdk.tar.gz
 
 # Permissions
-chmod 777 jdk
+chmod o+w jdk
 
 # Build
 pushd jdk
